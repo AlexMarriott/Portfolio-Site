422f1806a720fc32ced4ff1f69f8c57b331da3ac
alex marriott <alex-marriott@live.co.uk>
2019-10-10 23:29:32 +0100

    Added the new subnets csv file to just let me actually have nice looking subnet tables.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 import os
+import pandas
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/subnets')
+def subnet():
+    df = pandas.read_csv('C:\\Users\\alex\\subnet.csv')
+    return render_template('table.html', csv=df)
 
 if __name__ == '__main__':
     app.run(host=os.environ['FLASK_RUN_HOST'], port=os.environ['FLASK_RUN_PORT'])
