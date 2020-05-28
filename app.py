c134a23169b888194524a35ee0266e20724e5fde
alexmarriott <alex-marriott@live.co.uk>
2020-05-28 16:32:51 +0100

    Working on the CSS and design of the website.

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def subnet():
     df = pandas.read_csv('C:\\Users\\alex\\subnet.csv')
     return render_template('table.html', csv=df)
 
+@app.route('/vlans')
+def vlans():
+    df = pandas.read_csv('/Users/alexmarriott/Documents/vlans.csv')
+    return render_template('vlans.html', csv=df)
 if __name__ == '__main__':
     app.run(host=os.environ['FLASK_RUN_HOST'], port=os.environ['FLASK_RUN_PORT'])
