2c19e3cb6588b0adc54b2f7c9026a673a7827e1f
alexmarriott <alex-marriott@live.co.uk>
2020-06-12 19:19:28 +0100

    adding logging to script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 id_rsa # Allow read access to the private key
@@ -14,6 +18,6 @@ ssh -tt apps@$IP -p $PORT <<EOF
   export MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
   export GITHUB_TOKEN=$GITHUB_TOKEN
 
-  python3 env_replace.py
-  docker-compose down && docker-compose up
+  python3 env_replace.py &
+  docker-compose down && docker-compose up &
 EOF
