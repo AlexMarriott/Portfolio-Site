3b89cf01d2d2fdf6389209bd6b1e79e1925040c7
alexmarriott <alex-marriott@live.co.uk>
2020-06-12 20:24:53 +0100

    changing the enviroment variable name to read in the correct vars

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ ssh-add id_rsa # Add the private key to SSH
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
-
+#export MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
+#export GITHUB_TOKEN=$GITHUB_TOKEN
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -tt app@$IP -p $PORT <<EOF
 
@@ -16,9 +17,8 @@ ssh -tt app@$IP -p $PORT <<EOF
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 
-  export MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
-  export GITHUB_TOKEN=$GITHUB_TOKEN
 
-  python3 env_replace.py &
+
+  python3 env_replace.py
   docker-compose down && docker-compose up &
 EOF
