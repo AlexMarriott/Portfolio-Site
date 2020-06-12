839b40c9e304d1c9af84ea0bef9fac27b675c59a
alexmarriott <alex-marriott@live.co.uk>
2020-06-12 19:58:44 +0100

    changed apps to app

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh -tt apps@$IP -p $PORT <<EOF
+ssh -tt app@$IP -p $PORT <<EOF
 
   cd $DEPLOY_DIR
   exec 3>&1 4>&2
