97a431f2def14ce479effb05c67765dca3c13f03
alexmarriott <alex-marriott@live.co.uk>
2020-06-12 20:36:09 +0100

    work around to get the tokes passed to the docker-compose files.

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,18 @@ ssh-add id_rsa # Add the private key to SSH
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
-#export MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
-#export GITHUB_TOKEN=$GITHUB_TOKEN
+MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
+GITHUB_TOKEN=$GITHUB_TOKEN
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -tt app@$IP -p $PORT <<EOF
 
-  cd $DEPLOY_DIR
-  exec 3>&1 4>&2
+cd $DEPLOY_DIR
+exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
+echo $GITHUB_TOKEN
+echo $MONGO_INITDB_ROOT_PASSWORD
 
-
-
-  python3 env_replace.py
+  python3 env_replace.py $MONGO_INITDB_ROOT_PASSWORD $GITHUB_TOKEN
   docker-compose down && docker-compose up &
 EOF
--- env_replace.py
+++ env_replace.py
@@ -4,17 +4,28 @@ This means that the env variables are not included on the website, reducing the 
 """
 from yaml import load, dump, Loader, YAMLError
 import os
-import fileinput
+import sys
+
+mongo_pw = sys.argv[1]
+token = sys.argv[2]
+
+
 with open("docker-compose.yaml", 'r') as stream:
     try:
         loaded = load(stream, Loader=Loader)
     except Exception as exc:
         print(exc)
 
-# Modify the fields from the dict
+# Modify the fields from the dict, ditry ass hack.
 for i in loaded['services']['mongo']['environment']:
-    loaded['services']['mongo']['environment'][i] = os.environ[i]
 
+    if loaded['services']['mongo']['environment'][i] == 'MONGO_INITDB_ROOT_PASSWORD':
+        loaded['services']['mongo']['environment'][i] = mongo_pw
+
+    elif loaded['services']['mongo']['environment'][i] == 'GITHUB_TOKEN':
+        loaded['services']['mongo']['environment'][i] = token
+    else:
+        continue
 
 # Save it again
 with open("app.yaml", 'w') as stream:
@@ -28,7 +39,7 @@ with open('mongo-init.js', 'r') as file :
   filedata = file.read()
 
 # Replace the target string
-filedata = filedata.replace('MONGO_INITDB_ROOT_PASSWORD', os.environ['MONGO_INITDB_ROOT_PASSWORD'])
+filedata = filedata.replace('MONGO_INITDB_ROOT_PASSWORD', mongo_pw)
 
 # Write the file out again
 with open('mongo-init.js', 'w') as file:
