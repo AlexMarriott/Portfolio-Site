b77022abc9dd80060447ebcde4e6fe684e768e14
alexmarriott <alex-marriott@live.co.uk>
2020-06-12 22:14:45 +0100

    TRYING TO LOAD IN THE ENV USING THE .ENV METHOD

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,9 @@ ssh-add id_rsa # Add the private key to SSH
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
-MONGO_INITDB_ROOT_PASSWORD=$MONGO_INITDB_ROOT_PASSWORD
-GITHUB_TOKEN=$GITHUB_TOKEN
+
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -tt app@$IP -p $PORT <<EOF
-
-cd $DEPLOY_DIR
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
-#echo $GITHUB_TOKEN
-#echo $MONGO_INITDB_ROOT_PASSWORD
-
-  python3 env_replace.py $MONGO_INITDB_ROOT_PASSWORD $GITHUB_TOKEN
+  cd $DEPLOY_DIR
   docker-compose down && docker-compose up &
 EOF
